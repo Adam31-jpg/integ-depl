e500d21f59c3a91f9fdabb079765cbc9f919a1bb
Adam-Hzi <71019269+Adam31-jpg@users.noreply.github.com>
2024-03-20 09:55:27 +0100

    add todo home page

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoHomeComponent } from './components/todo-home/todo-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoHomeComponent
   ],
   imports: [
     BrowserModule,
